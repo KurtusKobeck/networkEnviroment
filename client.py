32c56ef83cf06a500bb57bf4d9e39f53d1f2fdbf
Kurtus Kobeck <kakobeck@gmail.com>
2024-10-19 20:09:19 -0700

    Reworked client.py to improve functionality and feature set
    Reworked client to be capable of both sending and receiving messages using threading.

--- client.py
+++ client.py
@@ -1,5 +1,23 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+from threading import Thread
+import os
+
+class Client:
+    def __init__(self, HOST, PORT):
+        self.socket=socket.socket()
+        self.socket.connect((HOST,PORT))
+        Thread(target = self.receiveResponseLoop).start()
+        self.sendCommandLoop()
+    def sendCommandLoop(self):
+        while True:
+            clientCommand = input("Enter your command: ")
+            self.socket.send(clientCommand.encode())
+    def receiveResponseLoop(self):
+        while True:
+            serverResponse = self.socket.recv(2048).decode()
+            if not serverResponse:
+                of._exit(0)
+            if len(serverResponse)>0:
+                print(serverResponse)
+if __name__ == '__main__':
+    Client(socket.gethostname(), 1234)
