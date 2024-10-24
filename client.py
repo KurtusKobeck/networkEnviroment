7afd1631388fbcd50fe7ce2d854ed7552b4512ba
Kurtus Kobeck <kakobeck@gmail.com>
2024-10-23 18:03:02 -0700

    Update client.py
    -Message receiving from the server is no longer threaded on the client
    -After sending a message to the server, the client enters receive mode. This ensures that the request for user input does not supercede the server's response. 
    -Added support for message tagging to resolve hangups caused by sending and receiving empty messages.

--- client.py
+++ client.py
@@ -6,18 +6,20 @@ class Client:
     def __init__(self, HOST, PORT):
         self.socket=socket.socket()
         self.socket.connect((HOST,PORT))
-        Thread(target = self.receiveResponseLoop).start()
+        #Thread(target = self.receiveResponseLoop).start()
         self.sendCommandLoop()
     def sendCommandLoop(self):
         while True:
-            clientCommand = input("Enter your command: ")
+            clientCommand = "0"+input("Enter your command: ")
             self.socket.send(clientCommand.encode())
+            self.receiveResponseLoop()
     def receiveResponseLoop(self):
         while True:
             serverResponse = self.socket.recv(2048).decode()
             if not serverResponse:
                 of._exit(0)
-            if len(serverResponse)>0:
-                print(serverResponse)
+            if len(serverResponse)>1:
+                print(serverResponse[1:])
+            return
 if __name__ == '__main__':
     Client(socket.gethostname(), 1234)
